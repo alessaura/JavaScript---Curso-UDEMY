cdc9ebab0243216ff394bf553662f0df29ab9893
Alessandra Sanches <als.sanches@outlook.com>
2023-05-02 11:10:24 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+sistema de cadasto de usuários
+*/
+let nome="Alessandra";
+let sobrenome="sanches";
+let idade= 23;
+const numCliente = 329482;
+
+idade= 24;
+let valorEmprestimo = 150000;
+let taxaDeJuros = 10;
+let ehBomPagador = "Sim";
+ehBomPagador = true;
+
+console.log("Boa tarde Sr. " + nome + " " + sobrenome + " ." + "Sua idade é:" + idade);
---
+++ supermercado.js
@@ -0,0 +1,8 @@
+/*
+sistema de cadasto de usuários
+*/
+let nomeProduto="Toddy";
+let preco = 6.00 ;
+let validade = true;
+
+console.log("O produto:" + nomeProduto + " custa: " + preco + " ,e sua validade é " + validade);
